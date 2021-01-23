e5a31aaf0c4a8ccd7876015adadacc58719215b1
mackristofer <mackristofer@gmail.com>
2021-01-22 23:34:59 -0300

    Implements endpoint methods findAll, findById

--- backend/src/main/java/br/bootcamp/modulo1/desafiomodulo1/repositories/ClientRepository.java
+++ backend/src/main/java/br/bootcamp/modulo1/desafiomodulo1/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package br.bootcamp.modulo1.desafiomodulo1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.bootcamp.modulo1.desafiomodulo1.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
--- backend/src/main/java/br/bootcamp/modulo1/desafiomodulo1/services/ClientService.java
+++ backend/src/main/java/br/bootcamp/modulo1/desafiomodulo1/services/ClientService.java
@@ -1,5 +1,36 @@
 package br.bootcamp.modulo1.desafiomodulo1.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import br.bootcamp.modulo1.desafiomodulo1.dto.ClientDTO;
+import br.bootcamp.modulo1.desafiomodulo1.entities.Client;
+import br.bootcamp.modulo1.desafiomodulo1.repositories.ClientRepository;
+
+
+@Service
 public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		//Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Object not found"));
+		return new ClientDTO(obj.get());
+	}
 
 }
